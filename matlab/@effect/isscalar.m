dbd8b0db9d72734696728ad47594692de1ff1b7c
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2020-04-20 14:18:21 -0700

    Cleaned up temporary matlab folder

--- matlab/@effect/isscalar.m
+++
@@ -1,4 +0,0 @@
-function out = isscalar(obj)
-% Check if data is scalar. 
-out = isscalar(obj.data);
-end
--- matlab/@effect/vertcat.m
+++
@@ -1,3 +0,0 @@
-function out = vertcat(obj)
-error('Vertical concatenation is not implemented.')
-end
