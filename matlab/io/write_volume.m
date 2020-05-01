e5e48c4e977c06b1d22e5bfa588b328c2b5208da
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2020-05-01 11:19:39 -0900

    [ENH] Initial I/O update.

---
+++ matlab/io/write_volume.m
@@ -0,0 +1,17 @@
+function write_volume(filename,vol,varargin)
+
+p = inputParser;
+addParameter(p,'voxelsize',[]);
+addParameter(p,'origin',[]);
+addParameter(p,'datatype',[])
+addParameter(p,'description',[])
+parse(p,varargin{:}); 
+
+if endsWith(filename,{'.nii','.nii.gz'})
+    nii = make_nii(vol, p.Results.voxelsize, p.Results.origin, ...
+        p.Results.datatype,p.Results.description);
+    save_nii(nii,filename)
+else
+    error('Only the writing of nifti files is supported.');
+end
+end
