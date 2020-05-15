a9a686842b6a8d4cb8a5a59561377780ce3ad03b
Şeyma Bayrak <sheymaba@gmail.com>
2020-05-15 16:04:32 +0200

    works for 3D array of shape (x,y,2)#

--- surfstat/python/SurfStatEdg.py
+++ surfstat/python/SurfStatEdg.py
@@ -36,55 +36,77 @@ def py_SurfStatEdge(surf):
 				
 				edg0 = np.block([[ c1, c1, c1, c2-1, c2-1, c2, c11, c21-I, c12, \
 				                c22-1 ], [ c1+1, c1+I, c1+1+I, c2, c2-1+I, c2-1+I, \
-				                c11+I, c21, c12+1, c22 ]]).T
+				                c11+I, c21, c12+1, c22 ]]).T +1
 				
 				edg1 = np.block([[ c1, c1, c1, c11, c11, c12, c12], [c1+IJ, c1+1+IJ, \
-								c1+I+IJ, c11+IJ, c11+I+IJ, c12+IJ, c12+1+IJ ]]).T
+								c1+I+IJ, c11+IJ, c11+I+IJ, c12+IJ, c12+1+IJ ]]).T +1
 				
 				edg2 = np.block([[c2-1, c2, c2-1+I, c21-I, c21, c22-1, c22], \
 				                [c2-1+IJ, c2-1+IJ, c2-1+IJ, c21-I+IJ, c21-I+IJ, \
-				                c22-1+IJ, c22-1+IJ]]).T
-				
-				
-			
-
+				                c22-1+IJ, c22-1+IJ]]).T +1
+				      
+			                
 				if f:
 					for k in range(2, K, 2):
 						edg[(k-1)*n1 + np.arange(0,n1), :]  = (np.block([[edg0], \
 						 [edg2], [edg1], [IJ, 2*IJ]]) + (k-1) *IJ) 
+				
+						print('AAAA ', k )
+						print((k-1) *IJ)
 				else:
 					for k in range(1,2,(K-1)):
 						edg[(k-1)*n1 + np.arange(0,n1), :]  = (np.block([[edg0], \
 						 [edg1], [edg2], [IJ, 2*IJ]]) + (k-1) *IJ) 
-						 
+										 
 				if np.remainder((K+1), 2) == f:
-					edg[(k-1)*n1 + np.arange(0,n2), :]  = edg0[np.arange(0,n2),:] \
-					+ (K-1) * IJ
-			
-			sys.exit('TO BE CONTINUED ON IMPLEMENTATION.....')
-			##% index by voxels in the lat
-			##vid=int32(cumsum(surf.lat(:)).*surf.lat(:));
-			##% only inside the lat
-			##edg=vid(edg(all(surf.lat(edg),2),:));
+					edg[ (k-1)*n1 + np.arange(0,n2) -1 , :]  = \
+					 edg0[np.arange(0,n2),:] + (K-1) * IJ
 					
+			
+			vid = np.array(np.multiply(np.cumsum(surf['lat'][:].T.flatten()), \
+			 surf['lat'][:].T.flatten()) , dtype='int')
+			vid = vid.reshape(len(vid),1)
+
+			all_idx = np.all(np.block([[surf['lat'].T.flatten()[edg[:,0] -1]], \
+			 [surf['lat'].T.flatten()[edg[:,1] -1]]]).T, axis=1)
+			 
+			edg = vid[edg[all_idx,:]-1].reshape(np.shape(edg[all_idx,:]-1))
+			
 	else:
 		sys.exit('surf must have "lat" or "tri" key !!!')
 
 	return edg
-	
-	
-
 
 #surf['tri'] = a
 
-a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
-Z = np.zeros((3,4,3))
+#a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+#Z = np.zeros((3,4,3))
+#Z[:,:,0] = a
+#Z[:,:,1] = a1          
+#Z[:,:,2] = a
+
+
+a = np.array([[9, 10]])
+Z = np.zeros((1,2,2))
 Z[:,:,0] = a
-Z[:,:,1] = a
-Z[:,:,2] = a
+Z[:,:,1] = a 
+
 
 surf = {}
 surf['lat'] = Z
 
 
-py_SurfStatEdge(surf)
+bla = py_SurfStatEdge(surf)
+
+print(bla)
+
+
+
+
+
+
+
+
+
+
+
