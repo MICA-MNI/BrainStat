53d6f025f4b9c7ad5be9c35423a33d1e6750807a
rcruces <raulrcruces@inb.unam.mx>
2020-05-20 19:56:42 -0400

    RRC comments added + @deprecated(BrainSpace dependency)

--- surfstat/python/need_not_convert/SurfStatColormap.py
+++ surfstat/python/need_not_convert/SurfStatColormap.py
@@ -1,4 +1,18 @@
+"""
+python version of SurfStatColormap
+"""
+
+# Author: RRC
+# License: BSD 3 clause
+
+
 import numpy as np
 
+@deprecated("BrainSpace dependency")
 def py_SurfStatColormap(map):
-    sys.exit("Function py_SurfStatColormap is not implemented yet")
+    """Colormap function for SurfStatView.
+    Usage: SurfStatColormap( map );
+    Same as for matlab's colormap function - see help colormap.
+    """
+
+    sys.exit("Function py_SurfStatColormap is now a BrainSpace dependency")
