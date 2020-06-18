e44803f7464386472d6de2e406148cccf5ea55fe
Şeyma Bayrak <sheymaba@gmail.com>
2020-06-18 13:06:10 +0200

    add more complex tests for SurfStatInd2Coord

--- surfstat/tests/test_SurfStatInd2Coord.py
+++ surfstat/tests/test_SurfStatInd2Coord.py
@@ -37,9 +37,92 @@ def test_surf_coord():
 def test_surf_lat_easy():
     # coord is 2D array, surf['lat'] is 3D array of ones
     m = np.random.randint(1,100)
+    n = np.random.randint(1,100)
  
     A = np.ones((1,m))
     B = {}
-    B['lat'] = np.ones((10,10,10))
+    B['lat'] = np.ones((n,n,n))
+    dummy_test(A, B)
+
+### Test 3 
+def test_surf_lat_hard():
+    # coord is 2D array, surf['lat'] is 3D array of ones
+    m = np.random.randint(1,100)
+    n = np.random.randint(1,100)
+ 
+    A = np.ones((1,m))
+    B = {}
+    B['lat'] = np.random.choice([0, 1], size=(n,n,n))
+    dummy_test(A, B)
+
+### Test 4 
+def test_surf_lat_complex():
+    # coord is 2D array, surf['lat'] is 3D array of ones
+    m = np.random.randint(1,100)
+    n = np.random.randint(1,100)
+    k = np.random.randint(1,100)
+    l = np.random.randint(1,100)
+ 
+    A = np.ones((1,m))
+    B = {}
+    B['lat'] = np.random.choice([0, 1], size=(n,l,k))
+    dummy_test(A, B)
+
+### Test 5 
+def test_surf_lat_vox_easy():
+    # coord is 2D array, surf['lat'] is 3D array of ones, surf['vox'] is 2D array
+    m = np.random.randint(1,100)
+    n = np.random.randint(1,100)
+    k = np.random.randint(1,100)
+    l = np.random.randint(1,100)
+
+    A = np.ones((1,m))
+    B = {}
+    B['lat'] = np.random.choice([0, 1], size=(n,n,n))
+    B['vox'] = np.array([[n,k,l]])
+    dummy_test(A, B)
+
+### Test 6 
+def test_surf_lat_vox_complex():
+    # coord is 2D array, surf['lat'] is 3D array of ones
+    m = np.random.randint(1,100)
+    n = np.random.randint(1,100)
+    k = np.random.randint(1,100)
+    l = np.random.randint(1,100)
+ 
+    A = np.ones((1,m))
+    B = {}
+    B['lat'] = np.random.choice([0, 1], size=(n,l,k))
+    B['vox'] = np.array([[n,k,l]])
+    dummy_test(A, B)
+
+### Test 7
+def test_surf_lat_vox_origin_easy():
+    # coord is 2D array, surf['lat'] 3D, surf['vox'] 2D, surf[orig] 2D 
+    m = np.random.randint(1,100)
+    n = np.random.randint(1,100)
+    k = np.random.randint(1,100)
+    l = np.random.randint(1,100)
+ 
+    A = np.ones((1,m))
+    B = {}
+    B['lat'] = np.random.choice([0, 1], size=(n,n,n))
+    B['vox'] = np.array([[n,k,l]])
+    B['origin'] = np.array([[k,l,n]])
+    dummy_test(A, B)
+
+### Test 8
+def test_surf_lat_vox_origin_hard():
+    # coord is 2D array, surf['lat'] 3D, surf['vox'] 2D, surf[orig] 2D 
+    m = np.random.randint(1,100)
+    n = np.random.randint(1,100)
+    k = np.random.randint(1,100)
+    l = np.random.randint(1,100)
+ 
+    A = np.ones((1,m))
+    B = {}
+    B['lat'] = np.random.choice([0, 1], size=(k,n,l))
+    B['vox'] = np.array([[n,k,l]])
+    B['origin'] = np.array([[k,l,n]])
     dummy_test(A, B)
 
