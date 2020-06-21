ea2c84d0e930b77452c9ddebecbaa22efa135abd
Şeyma Bayrak <sheymaba@gmail.com>
2020-06-21 12:46:50 +0200

    more complex tests added for SurfStatF.py

--- surfstat/tests/test_SurfStatF.py
+++ surfstat/tests/test_SurfStatF.py
@@ -27,8 +27,6 @@ def dummy_test(A, B):
 					   		     rtol=1e-05, equal_nan=True))
 	assert all(flag == True for (flag) in testout_SurfStatF)
 
-
-
 #### Test 1 
 def test_slm1_slm2_easy_int():
 
@@ -167,4 +165,109 @@ def test_slm1_slm2_coef3D_int_k2():
 
     dummy_test(slm1, slm2)
 
+#### Test 6
+def test_slm1_slm2_coef3D_int_k1():
+    
+    # k = 1
+    # slm1['coef'] is 3D array of integers
+    # slm1['X'] and slm2['X'] are the SAME, 2D arrays of integers
+    
+    rng = np.random.default_rng()
+    
+    n = np.random.randint(3,100)
+    p = np.random.randint(3,100)
+    k = 2
+    v = np.random.randint(3,100)
+
+    slm1 = {}
+    slm1['X'] = rng.integers(100, size=(n,p)) 
+    slm1['df'] = n
+    slm1['SSE'] =  rng.integers(1,100, size=(int(k*(k+1)/2),v))
+    slm1['coef'] = rng.integers(1,100, size=(p,v,k))
+
+    slm2 = {}
+    slm2['X'] = slm1['X']
+    slm2['df'] = p
+    slm2['SSE'] = rng.integers(1,100, size=(int(k*(k+1)/2),v))
+    slm2['coef'] = rng.integers(1,100, size=(p,v,k))
+
+    dummy_test(slm1, slm2)
+
+#### Test 7
+def test_slm1_slm2_coef3D_random_k3():
+
+    # k= 3
+    # slm1['coef'] is 3D random array
+    # slm1['X'] and slm2['X'] are the SAME, 2D random array
+    
+    n = np.random.randint(3,100)
+    p = np.random.randint(3,100)
+    k = 3
+    v = np.random.randint(3,100)
+
+    slm1 = {}
+    slm1['X'] = np.random.rand(n,p)
+    slm1['df'] = p
+    slm1['SSE'] = np.random.rand( int(k*(k+1)/2),v)
+    slm1['coef'] = np.random.rand(p,v,k)
+
+    slm2 = {}
+    slm2['X'] = slm1['X']
+    slm2['df'] = p+1
+    slm2['SSE'] =  np.random.rand( int(k*(k+1)/2),v)
+    slm2['coef'] = np.random.rand(p,v,k)
+       
+    dummy_test(slm1, slm2)
+    
+#### Test 8 
+def test_slm1_slm2_coef3D_random_k2():
+    
+    # k = 2
+    # slm1['coef'] is 3D random array
+    # slm1['X'] and slm2['X'] are the SAME, 2D random array
+    
+    n = np.random.randint(3,100)
+    p = np.random.randint(3,100)
+    k = 2
+    v = np.random.randint(3,100)
+
+    slm1 = {}
+    slm1['X'] = np.random.rand(n,p)
+    slm1['df'] = p+1
+    slm1['SSE'] = np.random.rand( int(k*(k+1)/2),v)
+    slm1['coef'] = np.random.rand(p,v,k)
+    
+    slm2 = {}
+    slm2['X'] = slm1['X']
+    slm2['df'] = p
+    slm2['SSE'] =  np.random.rand( int(k*(k+1)/2),v)
+    slm2['coef'] = np.random.rand(p,v,k)
+
+    dummy_test(slm1, slm2)
+    
+#### Test 9
+def test_slm1_slm2_coef3D_random_k1():
+    
+    # k = 1
+    # slm1['coef'] is 3D random array
+    # slm1['X'] and slm2['X'] are the SAME, 2D random array
+    
+    n = np.random.randint(3,100)
+    p = np.random.randint(3,100)
+    k = 1
+    v = np.random.randint(3,100)
+
+    slm1 = {}
+    slm1['X'] = np.random.rand(n,p)
+    slm1['df'] = p+1
+    slm1['SSE'] = np.random.rand( int(k*(k+1)/2),v)
+    slm1['coef'] = np.random.rand(p,v,k)
+    
+    slm2 = {}
+    slm2['X'] = slm1['X']
+    slm2['df'] = p
+    slm2['SSE'] =  np.random.rand( int(k*(k+1)/2),v)
+    slm2['coef'] = np.random.rand(p,v,k)
+
+    dummy_test(slm1, slm2)
 
