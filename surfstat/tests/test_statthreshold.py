583af839b1e054d07d7d66262fcdc83b349d2940
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2020-08-27 12:32:08 -0400

    [FIX] Fixed test_17 bug, added input error check.

--- surfstat/tests/test_statthreshold.py
+++ surfstat/tests/test_statthreshold.py
@@ -341,7 +341,7 @@ def test_11():
     num_voxels        = rng.integers(1,100, size=(k))
     fwhm              = np.random.uniform(0,10)
     df                = k
-    p_val_peak        = np.random.rand(k,1)
+    p_val_peak        = np.random.rand(k)
     cluster_threshold = 0.001
     p_val_extent      = 0.05
     nconj             = 0.5
@@ -366,7 +366,7 @@ def test_12():
     num_voxels        = rng.integers(1,100, size=(m))
     fwhm              = np.random.uniform(0,10)
     df                = k
-    p_val_peak        = np.random.rand(k,1)
+    p_val_peak        = np.random.rand(k)
     cluster_threshold = np.random.uniform(0,1)
     p_val_extent      = 0.05
     nconj             = 0.5
@@ -415,9 +415,9 @@ def test_14():
     num_voxels        = k
     fwhm              = np.random.uniform(0,10)
     df                = np.random.randint(1,(m-1))
-    p_val_peak        = np.random.rand(k,1)
+    p_val_peak        = np.random.rand(k)
     cluster_threshold = np.random.uniform(0,1)
-    p_val_extent      = np.random.rand(k,1)
+    p_val_extent      = np.random.rand(k)
     nconj             = l
     nvar              = 1
     EC_file           = None
@@ -472,9 +472,9 @@ def test_16():
 # buggy case 
 def test_17():
     somedata = loadmat('./tests/data/varA.mat')
-    varA = somedata['varA']
+    varA = somedata['varA'][0]
     df = somedata['df']
-    k = somedata['k']
+    k = somedata['k'][0]
     
     # this is from some real test data
     search_volume     = 0
