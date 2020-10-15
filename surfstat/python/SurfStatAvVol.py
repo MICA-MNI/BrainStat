91c32ed315d1dcf65e8c8a2e308612fb0c679b7d
Şeyma Bayrak <sheymaba@gmail.com>
2020-10-15 02:08:56 +0200

    make input filenames 1D-list instead of nested-list

--- surfstat/python/SurfStatAvVol.py
+++ surfstat/python/SurfStatAvVol.py
@@ -2,14 +2,13 @@ import numpy as np
 import nibabel as nb
 
 
-def py_SurfStatAvVol(filenames, fun = np.add, replace_nan = np.nan):
+def py_SurfStatAvVol(filenames, fun = np.add, Nan = None):
 
     #filenames = file name with extension .mnc, .img, .nii or .brik as above 
     #        (n=1), or n x 1 cell array of file names.
 
     n = np.shape(filenames)[0]
-
-    file_01 = filenames[0][0]
+    file_01 = filenames[0]
 
     if file_01.endswith('.nii') or file_01.endswith('.nii.gz') or \
         file_01.endswith('.img'):
@@ -19,10 +18,10 @@ def py_SurfStatAvVol(filenames, fun = np.add, replace_nan = np.nan):
             m = 1
         else:
             for i in range(0, n):
-                d = nb.load(filenames[i][0])
+                d = nb.load(filenames[i])
                 data = np.array(d.get_fdata())
 
-                if replace_nan != np.nan:
+                if Nan is not None:
                     data[np.isnan(data)] = replace_nan
 
                 if i == 0:
@@ -39,5 +38,6 @@ def py_SurfStatAvVol(filenames, fun = np.add, replace_nan = np.nan):
     vol['vox'] = np.array(d.header.get_zooms()[0:3])
     vol['origin'] = d.header['origin'][0:3]
     
-    return data, vol
+    return data_i, vol
+
 
