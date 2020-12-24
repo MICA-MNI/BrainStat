e5243393085e6cd180d0adc3f4272591c914e634
Şeyma Bayrak <sheymaba@gmail.com>
2020-12-24 18:49:47 +0100

    commentary for test data added in test_SurfStatF.py and test_SurfStatLinMod.py

--- brainstat/tests/test_SurfStatF.py
+++ brainstat/tests/test_SurfStatF.py
@@ -70,74 +70,214 @@ def dummy_test(infile, expfile):
 
     assert all(flag == True for (flag) in testout)
 
+# test data *pkl consists of slm1* and slm2* keys
+
 
 def test_01():
+    # slm1X : numpy array, shape (5, 6), int64
+    # slm1df  : int
+    # slm1SSE : numpy array, shape (3, 1), int64
+    # slm1coef : numpy array, shape (6, 1), int64
+    # slm2X' : numpy array, shape (5, 6), int64
+    # slm2df' : int,
+    # slm2SSE' : numpy array, shape (3, 1), int64
+    # slm2coef' : numpy array, shape (6, 1), int64
     infile  = datadir('statf_01_IN.pkl')
     expfile = datadir('statf_01_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_02():
+    # slm1X : numpy array, shape (84, 77), int64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (1128, 42), int64
+    # slm1coef : numpy array, shape (77, 42), int64
+    # slm2X : numpy array, shape (84, 77), int64
+    # slm2df : int
+    # slm2SSE : numpy array, shape (1128, 42), int64
+    # slm2coef : numpy array, shape (77, 42), int64
     infile  = datadir('statf_02_IN.pkl')
     expfile = datadir('statf_02_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_03():
+    # slm1X numpy array, shape (91, 58), float64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (2278, 75), float64
+    # slm1coef : numpy array, shape (58, 75), float64
+    # slm2X : numpy array, shape (91, 58), float64
+    # slm2df : int
+    # slm2SSE : numpy array, shape (2278, 75), float64
+    # slm2coef : numpy array, shape (58, 75) float64
     infile  = datadir('statf_03_IN.pkl')
     expfile = datadir('statf_03_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_04():
+    # slm1X : numpy array, shape (19, 27), int64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (6, 87), int64
+    # slm1coef : numpy array, shape (27, 87, 3), float64
+    # slm2X : numpy array, shape (19, 27), int64
+    # slm2df : int
+    # slm2SSE : numpy array, shape (6, 87), int64
+    # slm2coef : numpy array, shape (27, 87, 3), float64
     infile  = datadir('statf_04_IN.pkl')
     expfile = datadir('statf_04_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_05():
+    # slm1X : numpy array, shape (13, 3), int64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (3, 27), int64
+    # slm1coef : numpy array, shape (3, 27, 2), float64
+    # slm2X : numpy array, shape (13, 3),  int64
+    # slm2df : int
+    # slm2SSE : numpy array, shape (3, 27), int64
+    # slm2coef numpy array, shape (3, 27, 2), float64
     infile  = datadir('statf_05_IN.pkl')
     expfile = datadir('statf_05_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_06():
+    # slm1X : numpy array, shape (13, 10), int64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (3, 34), int64
+    # slm1coef : numpy array, shape (10, 34, 2), int64
+    # slm2X : numpy array, shape (13, 10), int64
+    # slm2df : int
+    # slm2SSE : numpy array, shape (3, 34), int64
+    # slm2coef : numpy array, shape (10, 34, 2), int64
     infile  = datadir('statf_06_IN.pkl')
     expfile = datadir('statf_06_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_07():
+    # slm1X : numpy array, shape (12, 4), float64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (6, 42), float64
+    # slm1coef : numpy array, shape (4, 42, 3), float64
+    # slm2X : numpy array, shape (12, 4), float64
+    # slm2df : int
+    # slm2SSE numpy array, shape (6, 42), float64
+    # slm2coef numpy array, shape (4, 42, 3), float64
     infile  = datadir('statf_07_IN.pkl')
     expfile = datadir('statf_07_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_08():
+    # slm1X : numpy array, shape (32, 91), float64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (3, 78), float64
+    # slm1coef : numpy array, shape (91, 78, 2), float64
+    # slm2X : numpy array, shape (32, 91), float64
+    # slm2df : int
+    # slm2SSE numpy array, shape (3, 78), float64
+    # slm2coef numpy array, shape (91, 78, 2), float64
     infile  = datadir('statf_08_IN.pkl')
     expfile = datadir('statf_08_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_09():
+    # slm1X : numpy array, shape (88, 49), float64
+    # slm1df : int
+    # slm1SSE : numpy array, shape (1, 56), float64
+    # slm1coef : numpy array, shape (49, 56, 1), float64
+    # slm2X : numpy array, shape (88, 49), float64
+    # slm2df : int
+    # slm2SSE : numpy array, shape (1, 56), float64
+    # slm2coef : numpy array, shape (49, 56, 1), float64
     infile  = datadir('statf_09_IN.pkl')
     expfile = datadir('statf_09_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_10():
+    # slm1X : numpy array, shape (10, 2), uint8
+    # slm1df : int
+    # slm1SSE : numpy array, shape (1, 20484), float64
+    # slm1coef : numpy array, shape (2, 20484), float64
+    # slm1tri : numpy array, shape (40960, 3), int32
+    # slm1resl : numpy array, shape (61440, 1), float64
+    # slm1c : numpy array, shape (1, 2), float64
+    # slm1k : int
+    # slm1ef : numpy array, shape (1, 20484), float64
+    # slm1sd : numpy array, shape (1, 20484), float64
+    # slm1t : numpy array, shape (1, 20484), float64
+    # slm2X : numpy array, shape (10, 2), uint8
+    # slm2df : int
+    # slm2SSE : numpy array, shape (1, 20484), float64
+    # slm2coef : numpy array, shape (2, 20484), float64
+    # slm2tri : numpy array, shape (40960, 3), int32
+    # slm2resl : numpy array, shape (61440, 1), float64
+    # slm2c : numpy array, shape (1, 2), float64
+    # slm2k : int
+    # slm2ef : numpy array, shape (1, 20484), float64
+    # slm2sd : numpy array, shape (1, 20484), float64
+    # slm2t : numpy array, shape (1, 20484), float64
     infile  = datadir('statf_10_IN.pkl')
     expfile = datadir('statf_10_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_11():
+    # slm1X : numpy array, shape (10, 2), uint8
+    # slm1df : int
+    # slm1SSE : numpy array, shape (1, 20484), float64
+    # slm1coef : numpy array, shape (2, 20484), float64
+    # slm1tri : numpy array, shape (40960, 3), int32
+    # slm1resl : numpy array, shape (61440, 1), float64
+    # slm1c : numpy array, shape (1, 2), float64
+    # slm1k : int
+    # slm1ef : numpy array, shape (1, 20484), float64
+    # slm1sd : numpy array, shape (1, 20484), float64
+    # slm1t : numpy array, shape (1, 20484), float64
+    # slm2X : numpy array, shape (10, 2), uint8
+    # slm2df : int
+    # slm2SSE : numpy array, shape (1, 20484), float64
+    # slm2coef : numpy array, shape (2, 20484), float64
+    # slm2tri : numpy array, shape (40960, 3), int32
+    # slm2resl : numpy array, shape (61440, 1), float64
+    # slm2c : numpy array, shape (1, 2), float64
+    # slm2k : int
+    # slm2ef : numpy array, shape (1, 20484), float64
+    # slm2sd : numpy array, shape (1, 20484), float64
+    # slm2t : numpy array, shape (1, 20484), float64
     infile  = datadir('statf_11_IN.pkl')
     expfile = datadir('statf_11_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_12():
+    # slm1X : numpy array, shape (20, 9), uint16
+    # slm1df : int
+    # slm1SSE : numpy array, shape (1, 20484), float64
+    # slm1coef : numpy array, shape (9, 20484), float64
+    # slm1tri : numpy array, shape (40960, 3), int32
+    # slm1resl : numpy array, shape (61440, 1), float64
+    # slm1c : numpy array, shape (1, 9), float64
+    # slm1k : int
+    # slm1ef : numpy array, shape (1, 20484), float64
+    # slm1sd : numpy array, shape (1, 20484), float64
+    # slm1t : numpy array, shape (1, 20484), float64
+    # slm2X : numpy array, shape (20, 9), uint16
+    # slm2df : int
+    # slm2SSE : numpy array, shape (1, 20484), float64
+    # slm2coef : numpy array, shape (9, 20484), float64
+    # slm2tri : numpy array, shape (40960, 3), int32
+    # slm2resl : numpy array, shape (61440, 1), float64
+    # slm2c : numpy array, shape (1, 9), float64
+    # slm2k : int
+    # slm2ef : numpy array, shape (1, 20484), float64
+    # slm2sd : numpy array, shape (1, 20484), float64
+    # slm2t : numpy array, shape (1, 20484), float64
     infile  = datadir('statf_12_IN.pkl')
     expfile = datadir('statf_12_OUT.pkl')
     dummy_test(infile, expfile)
--- brainstat/tests/test_SurfStatLinMod.py
+++ brainstat/tests/test_SurfStatLinMod.py
@@ -18,247 +18,228 @@ def dummy_test(slm, oslm):
     assert all(flag == True for (flag) in testout)
 
 
-def test_01():
 
+def test_01():
+    # ['Y'] : numpy array, shape (43, 43), dtype('float64')
+    # ['M'] : numpy array, (43, 43), dtype('float64')
     infile  = datadir('linmod_01_IN.pkl')
     expfile = datadir('linmod_01_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y   = idic['Y']
     M   = idic['M']
     slm = SurfStatLinMod(Y, M)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_02():
-
+    # ['Y'] : numpy array, (62, 7), dtype('float64')
+    # ['M'] : numpy array, (62, 92), dtype('float64')
     infile  = datadir('linmod_02_IN.pkl')
     expfile = datadir('linmod_02_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y   = idic['Y']
     M   = idic['M']
     slm = SurfStatLinMod(Y, M)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_03():
-
+    # ['Y'] : numpy array, (52, 64, 76), dtype('float64')
+    # ['M'] : numpy array, (52, 2), dtype('float64')
     infile  = datadir('linmod_03_IN.pkl')
     expfile = datadir('linmod_03_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y   = idic['Y']
     M   = idic['M']
     slm = SurfStatLinMod(Y, M)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_04():
-
+    # ['Y'] : numpy array, (69, 41, 5), dtype('float64')
+    # ['M'] : numpy array, (69, 30), dtype('float64')
     infile  = datadir('linmod_04_IN.pkl')
     expfile = datadir('linmod_04_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y   = idic['Y']
     M   = idic['M']
     slm = SurfStatLinMod(Y, M)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_05():
-
+    # ['Y'] : numpy array, (81, 1), dtype('float64')
+    # ['M'] : numpy array, (81, 2), dtype('float64')
     infile  = datadir('linmod_05_IN.pkl')
     expfile = datadir('linmod_05_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y   = idic['Y']
     M   = idic['M']
     slm = SurfStatLinMod(Y, M)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_06():
-
+    # ['Y'] : numpy array, (93, 41, 57), dtype('float64')
+    # ['M'] : numpy array, (93, 67), dtype('float64')
     infile  = datadir('linmod_06_IN.pkl')
     expfile = datadir('linmod_06_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y   = idic['Y']
     M   = idic['M']
     M   = Term(M)
     slm = SurfStatLinMod(Y, M)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_07():
-
+    # ['Y'] : numpy array, (40, 46, 21), dtype('float64')
+    # ['M'] : numpy array, (40, 81), dtype('float64')
     infile  = datadir('linmod_07_IN.pkl')
     expfile = datadir('linmod_07_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y   = idic['Y']
     M   = idic['M']
     M   = Term(M)
     slm = SurfStatLinMod(Y, M)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_08():
-
+    # ['Y'] : numpy array, (93, 43), dtype('float64')
+    # ['M'] : numpy array, (93, 2), dtype('float64')
+    # ['tri'] : numpy array, (93, 3), dtype('int64')
     infile  = datadir('linmod_08_IN.pkl')
     expfile = datadir('linmod_08_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y    = idic['Y']
     M    = idic['M']
     surf = {}
     surf['tri'] = idic['tri']
     slm = SurfStatLinMod(Y, M, surf)
-
+    # oslm : expected output
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_09():
-
+    # ['Y'] : numpy array, (98, 69, 60), dtype('float64')
+    # ['M'] : numpy array, (98, 91), dtype('float64')
+    # ['tri'] : numpy array, (60, 3), dtype('int64')
     infile  = datadir('linmod_09_IN.pkl')
     expfile = datadir('linmod_09_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y    = idic['Y']
     M    = idic['M']
     M    = Term(M)
     surf = {}
     surf['tri'] = idic['tri']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_10():
-
+    # ['Y'] : numpy array, (49, 27), dtype('float64')
+    # ['M'] : numpy array, (49, 2), dtype('float64')
+    # ['lat'] : numpy array, (3, 3, 3), dtype('bool')
     infile  = datadir('linmod_10_IN.pkl')
     expfile = datadir('linmod_10_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y    = idic['Y']
     M    = idic['M']
     surf = {}
     surf['lat'] = idic['lat']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_11():
-
+    # ['Y'] : numpy array, (45, 27, 3), dtype('float64')
+    # ['M'] : numpy array, (45, 7), dtype('float64')
+    # ['lat'] : numpy array, (3, 3, 3), dtype('int64')
     infile  = datadir('linmod_11_IN.pkl')
     expfile = datadir('linmod_11_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y    = idic['Y']
     M    = idic['M']
     M    = Term(M)
     surf = {}
     surf['lat'] = idic['lat']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_12():
-
+    # ['Y'] : numpy array, (10, 20484), dtype('float64')
+    # ['age'] : numpy array, (1, 10), dtype('float64')
+    # ['tri'] : numpy array, (40960, 3), dtype('int32')
+    # ['coord'] :numpy array, (3, 20484), dtype('float64')
     infile  = datadir('linmod_12_IN.pkl')
     expfile = datadir('linmod_12_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y    = idic['Y']
     age  = idic['age']
     AGE  = Term(np.array(age), 'AGE')
@@ -267,23 +248,22 @@ def test_12():
     surf['tri'] = idic['tri']
     surf['coord'] = idic['coord']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_13():
-
+    # ['Y'] : numpy array, (10, 20484), dtype('float64')
+    # ['age'] : numpy array, (1, 10), dtype('float64')
+    # ['tri'] : numpy array, (40960, 3), dtype('int32')
+    # ['coord'] : numpy array, (3, 20484), dtype('float64')
     infile  = datadir('linmod_13_IN.pkl')
     expfile = datadir('linmod_13_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y    = idic['Y']
     age  = idic['age']
     AGE  = Term(np.array(age), 'AGE')
@@ -292,23 +272,22 @@ def test_13():
     surf['tri'] = idic['tri']
     surf['coord'] = idic['coord']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_14():
-
+    # ['Y'] : numpy array, (10, 20484), dtype('float64')
+    # ['age'] : numpy array, (1, 10), dtype('float64')
+    # ['tri'] : numpy array, (40960, 3), dtype('int32')
+    # ['coord'] : numpy array, (3, 20484), dtype('float64')
     infile  = datadir('linmod_14_IN.pkl')
     expfile = datadir('linmod_14_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y    = idic['Y']
     age  = idic['age']
     AGE  = Term(np.array(age), 'AGE')
@@ -317,62 +296,58 @@ def test_14():
     surf['tri'] = idic['tri']
     surf['coord'] = idic['coord']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_15():
-
+    # building the model term while testing..
+    # ['Y'] : numpy array, (20, 20484), dtype('float64')
+    # ['params'] : numpy array, (20, 9), dtype('uint16')
+    # ['colnames'] : numpy array, (9,), dtype('<U11')
+    # ['tri'] : numpy array, (40960, 3), dtype('int32')
+    # ['coord'] : numpy array, (3, 20484), dtype('float64')
     infile  = datadir('linmod_15_IN.pkl')
     expfile = datadir('linmod_15_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y        = idic['Y']
     params   = idic['params']
     colnames = list(idic['colnames'])
     M        = Term(params, colnames)
-
     surf = {}
     surf['tri'] = idic['tri']
     surf['coord'] = idic['coord']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
 def test_16():
-
+    # ['Y'] : numpy array, (20, 20484), dtype('float64')
+    # ['params'] : numpy array, (20, 9), dtype('uint16')
+    # ['tri'] : numpy array, (40960, 3), dtype('int32')
+    # ['coord'] :numpy array, (3, 20484), dtype('float64')
     infile  = datadir('linmod_16_IN.pkl')
     expfile = datadir('linmod_16_OUT.pkl')
-
     ifile = open(infile, 'br')
     idic  = pickle.load(ifile)
     ifile.close()
-
     Y        = idic['Y']
     params   = idic['params']
     M        = Term(params)
-
     surf = {}
     surf['tri'] = idic['tri']
     surf['coord'] = idic['coord']
     slm = SurfStatLinMod(Y, M, surf)
-
     ofile = open(expfile, 'br')
     oslm  = pickle.load(ofile)
     ofile.close()
-
     dummy_test(slm, oslm)
 
 
