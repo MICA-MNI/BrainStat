46517435d693c65f6c51317f2608301ff142d72c
Şeyma Bayrak <sheymaba@gmail.com>
2020-12-24 14:58:20 +0100

    commentary for test data added in test_SurfStatEdg.py

--- brainstat/tests/test_SurfStatEdg.py
+++ brainstat/tests/test_SurfStatEdg.py
@@ -36,38 +36,47 @@ def dummy_test(infile, expfile):
 
     assert all(flag == True for (flag) in testout)
 
+# data *pkl consists of either keys ['tri'] or ['lat'], which will be assigned to
+# the surf{} dictionary while testing
+
 
 def test_01():
+    # ['tri'] is a 2D numpy array of shape (78, 3), dtype('float64')
     infile  = datadir('statedg_01_IN.pkl')
     expfile = datadir('statedg_01_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_02():
+    # ['lat'] is a 2D numpy array of shape (10, 10), dtype('float64')
     infile  = datadir('statedg_02_IN.pkl')
     expfile = datadir('statedg_02_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_03():
+    # ['lat'] is a 3D numpy array of shape (10, 10, 10), dtype('int64')
     infile  = datadir('statedg_03_IN.pkl')
     expfile = datadir('statedg_03_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_04():
+    # ['tri'] is a 2D numpy array of shape (40960, 3), dtype('int32')
     infile  = datadir('statedg_04_IN.pkl')
     expfile = datadir('statedg_04_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_05():
+    # ['tri'] is a 2D numpy array of shape (40960, 3), dtype('int32')
     infile  = datadir('statedg_05_IN.pkl')
     expfile = datadir('statedg_05_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_06():
+    # ['tri'] is a 2D numpy array of shape (2044, 3), dtype('uint16')
     infile  = datadir('statedg_06_IN.pkl')
     expfile = datadir('statedg_06_OUT.pkl')
     dummy_test(infile, expfile)
