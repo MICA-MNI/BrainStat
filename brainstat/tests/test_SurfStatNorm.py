c9d5e0d6f35d46e9358a25ef769180b12efaa35e
Şeyma Bayrak <sheymaba@gmail.com>
2020-12-26 13:32:56 +0100

    test explanations added in test_SurfStatNorm.py

--- brainstat/tests/test_SurfStatNorm.py
+++ brainstat/tests/test_SurfStatNorm.py
@@ -37,31 +37,35 @@ def dummy_test(infile, expfile):
 
 
 def test_01():
-    # ['Y'] : numpy array, shape (1, 2), dtype('int64')
+    # testing only with mandatory input ['Y']
+    # ['Y'] : 2D np array, shape (1, 2), dtype('int64')
     infile  = datadir('statnor_01_IN.pkl')
     expfile = datadir('statnor_01_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_02():
-    # ['Y'] : numpy array, shape (1, 10), dtype('int64')
-    # ['mask'] : numpy darray, shape (10,), dtype('bool')
+    # testing with optional input ['mask']
+    # ['Y'] : 2D np array, shape (1, 10), dtype('int64')
+    # ['mask'] : 1D np array, shape (10,), dtype('bool')
     infile  = datadir('statnor_02_IN.pkl')
     expfile = datadir('statnor_02_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_03():
-    # ['Y'] : numpy array, shape (2, 10), dtype('int64')
-    # ['mask'] : numpy darray, shape (10,), dtype('bool')
+    # increase size of ['Y'], keep optional input ['mask']
+    # ['Y'] : 2D np array, shape (2, 10), dtype('int64')
+    # ['mask'] : 1D np array, shape (10,), dtype('bool')
     infile  = datadir('statnor_03_IN.pkl')
     expfile = datadir('statnor_03_OUT.pkl')
     dummy_test(infile, expfile)
 
 
 def test_04():
-    # ['Y'] : numpy array, shape (3, 4, 2), dtype('float64')
-    # ['mask'] : numpy darray, shape (4,), dtype('bool')
+    # increase dimension of ['Y'], keep optional input ['mask']
+    # ['Y'] : 3D np array, shape (3, 4, 2), dtype('float64')
+    # ['mask'] : 1D np array, shape (4,), dtype('bool')
     infile  = datadir('statnor_04_IN.pkl')
     expfile = datadir('statnor_04_OUT.pkl')
     dummy_test(infile, expfile)
