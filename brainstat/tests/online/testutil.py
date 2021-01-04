e80f93ffe4b6e6972764b3c594bb2237f24897fb
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-01-04 12:48:19 -0500

    [FIX] Fixed test file location.

--- brainstat/tests/online/testutil.py
+++ brainstat/tests/online/testutil.py
@@ -3,6 +3,6 @@ import os
 
 def datadir(file):
     topdir = os.path.dirname(__file__)
-    topdir = os.path.join(topdir, "../..")
+    topdir = os.path.join(topdir, "../../../")
     topdir = os.path.abspath(topdir)
     return os.path.join(topdir, "extern/test-data", file)
