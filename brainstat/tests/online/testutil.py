933f13170d861064402bdc491fdf3b5613651797
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-01-09 09:26:27 -0500

    [ENH] Added tutorial directory, moved tests

--- brainstat/tests/online/testutil.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def datadir(file):
-    topdir = os.path.dirname(__file__)
-    topdir = os.path.join(topdir, "../../../")
-    topdir = os.path.abspath(topdir)
-    return os.path.join(topdir, "extern/test-data", file)
