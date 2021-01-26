86851a6eac0b444eef9bd2ed0a2ad304b93209b0
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-01-26 12:51:53 -0500

    [FIX] Flake8 issues

--- brainstat/context/meta_analysis.py
+++ brainstat/context/meta_analysis.py
@@ -10,16 +10,17 @@ from nimare.tests.utils import get_test_data_path
 from .utils import mutli_surface_to_volume
 from pathlib import Path
 
+
 def surface_decode(
-                pial,
-                white,
-                labels,
-                threshold, 
-                interpolation='linear',
-                decoder='neurosynth',
-                data_dir=None,
-                verbose=True,
-                correction='fdr_bh'):
+        pial,
+        white,
+        labels,
+        threshold,
+        interpolation='linear',
+        decoder='neurosynth',
+        data_dir=None,
+        verbose=True,
+        correction='fdr_bh'):
     """Meta-analytic decoding of surface maps using NeuroSynth or Brainmap.
 
     Parameters
@@ -35,7 +36,7 @@ def surface_decode(
         labels, or a list containing multiple of the aforementioned.
     threshold : float, int
         Value at which to threshold the labels in volume space. Voxels below
-        this value are set to 0. 
+        this value are set to 0.
     interpolation : str
         Either 'nearest' for nearest neighbor interpolation, or 'linear'
         for trilinear interpolation, by default 'linear'.
@@ -44,17 +45,17 @@ def surface_decode(
         brainmap decoder, by default 'neurosynth'.
     data_dir : str, optional
         The directory of the nimare dataset. If none exists, a new dataset will
-        be downloaded and saved to this path. If None, the directory defaults to 
+        be downloaded and saved to this path. If None, the directory defaults to
         your home directory, by default None
     verbose : bool, optional
         If true prints additional output to the console, by default True
     correction : str, optional
-        Multiple comparison correction. Valid options are None and 'fdr_bh' , 
+        Multiple comparison correction. Valid options are None and 'fdr_bh',
         by default 'fdr_bh'
 
     Returns
     -------
-    pandas.DataFrame 
+    pandas.DataFrame
         Table with each label and the following values associated with each
         label: ‘pForward’, ‘zForward’, ‘likelihoodForward’,‘pReverse’,
         ‘zReverse’, and ‘probReverse’.
@@ -66,7 +67,7 @@ def surface_decode(
     dset = fetch_dataset(data_dir)
 
     nii = mutli_surface_to_volume(pial, white, dset.masker.mask_img,
-        labels, verbose=verbose, interpolation=interpolation)
+                                  labels, verbose=verbose, interpolation=interpolation)
 
     ids = dset.get_studies_by_mask(nii.get_fdata() > threshold)
 
@@ -81,13 +82,14 @@ def surface_decode(
     decoded_df.sort_values(by="probReverse", ascending=False).head()
     return decoded_df
 
+
 def fetch_dataset(data_dir, keep_neurosynth=False):
     """Downloads the nimare dataset and fetches its path.
 
     Parameters
     ----------
     data_dir : str
-        Path to the directory where the dataset will be saved. 
+        Path to the directory where the dataset will be saved.
     keep_neurosynth : bool, optional
         If true, then the neurosynth data files are kept, by default False.
 
@@ -101,7 +103,6 @@ def fetch_dataset(data_dir, keep_neurosynth=False):
         dset = nimare.dataset.Dataset.load(os.path.join(
             data_dir, "neurosynth_nimare_with_abstracts.pkl.gz"))
         return dset
-
 
     if not os.path.isdir(data_dir):
         os.mkdir(data_dir)
@@ -118,7 +119,7 @@ def fetch_dataset(data_dir, keep_neurosynth=False):
         os.path.join(ns_dir, "database.txt"), os.path.join(ns_dir, "features.txt"))
     dset = nimare.extract.download_abstracts(dset, "tsalo006@fiu.edu")
     dset.save(os.path.join(data_dir, "neurosynth_nimare_with_abstracts.pkl.gz"))
-    
+
     if not keep_neurosynth:
         D.cleanup()
 
--- brainstat/context/utils.py
+++ brainstat/context/utils.py
@@ -10,9 +10,10 @@ from brainspace.mesh.mesh_io import read_surface
 from brainspace.vtk_interface.wrappers.data_object import BSPolyData
 from brainstat.mesh.interpolation import surface_to_volume
 
-def mutli_surface_to_volume(pial, white, volume_template, labels, interpolation='nearest', 
-    verbose=True):
-    """Interpolates multiple surfaces to the volume. 
+
+def mutli_surface_to_volume(pial, white, volume_template, labels, interpolation='nearest',
+                            verbose=True):
+    """Interpolates multiple surfaces to the volume.
 
     Parameters
     ----------
@@ -32,7 +33,7 @@ def mutli_surface_to_volume(pial, white, volume_template, labels, interpolation=
         Either 'nearest' for nearest neighbor interpolation, or 'linear'
         for trilinear interpolation, defaults to 'nearest'.
     verbose : boolean
-        If true, returns verbose output to console, defaults to true. 
+        If true, returns verbose output to console, defaults to true.
 
     Returns
     -------
@@ -45,7 +46,6 @@ def mutli_surface_to_volume(pial, white, volume_template, labels, interpolation=
     parcellations overlap across surfaces, then the labels are kept for the
     first provided surface.
     """
-
 
     # Deal with variety of ways to provide input.
     if type(pial) is not type(white):
@@ -72,16 +72,16 @@ def mutli_surface_to_volume(pial, white, volume_template, labels, interpolation=
         volume_template = nib.load(volume_template)
 
     for i in range(len(labels)):
-        if not isinstance(labels[i],np.ndarray):
+        if not isinstance(labels[i], np.ndarray):
             labels[i] = load_mesh_labels(labels[i])
 
     # Surface data to volume.
     T = []
     for i in range(len(pial)):
         T.append(tempfile.NamedTemporaryFile(suffix='.nii.gz'))
-        surface_to_volume(pial[i], white[i], labels[i], volume_template, T[i].name, 
-            interpolation=interpolation, verbose=verbose>0)
-    
+        surface_to_volume(pial[i], white[i], labels[i], volume_template, T[i].name,
+                          interpolation=interpolation, verbose=verbose > 0)
+
     if len(T) > 1:
         P = tempfile.TemporaryFile(suffix='.nii.gz')
         P_name = P.name
@@ -116,7 +116,7 @@ def combine_parcellations(files, output_file):
             affine = nii.affine
             header = nii.header
         else:
-            img[img==0] = nii.get_fdata()[img==0]
+            img[img == 0] = nii.get_fdata()[img == 0]
     new_nii = nib.Nifti1Image(img, affine, header)
     nib.save(new_nii, output_file)
 
