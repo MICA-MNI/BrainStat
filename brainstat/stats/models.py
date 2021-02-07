4fc3920def62559c84b9e7d28beb4b5412b1a442
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-02-07 17:37:23 -0500

    [FIX] Fixed mixed effects t-test (#94)
    * [FIX] Fixed a float being used for indexing.
    
    * [GIT] Trigger CI.
    
    * [REF] Black formatting

--- brainstat/stats/models.py
+++ brainstat/stats/models.py
@@ -1,5 +1,6 @@
 """Creation, fitting, and comparison of linear models."""
 import warnings
+import math
 import numpy as np
 import numpy.linalg as la
 import scipy
@@ -531,12 +532,13 @@ def t_test(slm, contrast):
             q1, v = np.shape(slm["r"])
             q = q1 + 1
             nc = np.shape(slm["dr"])[1]
-            chunck = np.ceil(v / nc)
+            chunk = math.ceil(v / nc)
             irs = np.zeros((q1, v))
 
             for ic in range(1, nc + 1):
-                v1 = 1 + (ic - 1) * chunck
-                v2 = np.min((v1 + chunck - 1, v))
+                v1 = 1 + (ic - 1) * chunk
+                v2 = np.min((v1 + chunk - 1, v))
+
                 vc = v2 - v1 + 1
 
                 irs[:, int(v1 - 1) : int(v2)] = np.around(
