202faa1a52febee545c401840d824a7c3aa4622b
Şeyma Bayrak <sheymaba@gmail.com>
2021-03-16 21:13:29 +0100

    test data for linear models generated

--- brainstat/tests/test_linear_model.py
+++ brainstat/tests/test_linear_model.py
@@ -1,134 +1,115 @@
 import numpy as np
-import pickle
 from .testutil import datadir
 from brainstat.stats.terms import Term
 from brainstat.stats.SLM import SLM
+import h5py
 
 
-def dummy_test(idic, oslm):
+def dummy_test(infile, expfile, simple=True):
 
     testout = []
 
-    slm = SLM(idic["M"], Term(1))
-    Y = idic["Y"]
-    if "tri" in idic.keys():
-        slm.surf = {"tri": idic["tri"]}
+    # infile includes at least 'Y'
+    hin = h5py.File(infile,'r')
+    Y = np.array(hin['Y'])
 
-    if "lat" in idic.keys():
-        slm.surf = {"lat": idic["lat"]}
-    if "coord" in idic.keys():
-        slm.surf["coord"] = idic["coord"]
+    # if simple, then, key 'M' is given
+    if simple:
+        M = np.array(hin['M'])
+    # otherwise, key 'AGE' is given
+    else:
+        M = np.array(hin['AGE'])
+        AGE = Term(M, "AGE")
+        M = 1 + AGE
 
+    slm = SLM(M, Term(1))
+
+    # infile might include 'tri', 'coord', 'lat' keys
+    for makey in hin.keys():
+        if makey == 'tri':
+            slm.surf = {'tri': np.array(hin['tri'])}
+        if makey == 'lat':
+            slm.surf = {'lat': np.array(hin['lat'])}
+        if makey == 'coord':
+            slm.surf = {'coord': np.array(hin['coord'])}
+
+    # here we go --> run the linear model
     slm.linear_model(Y)
-    for key in oslm.keys():
-        comp = np.allclose(getattr(slm, key), oslm[key], rtol=1e-05, equal_nan=True)
-        testout.append(comp)
+
+    # expfile includes the expected output of the linear model
+    hexp = h5py.File(expfile,'r')
+
+    # compare...
+    for makey_ in hexp.keys():
+        if makey_ != 'surf':
+            comp = np.allclose(getattr(slm, makey_), hexp[makey_],
+                               rtol=1e-05, equal_nan=True)
+            print(comp)
+            testout.append(comp)
     assert all(flag == True for (flag) in testout)
 
 
 def test_01():
-    # ['Y'] and ['M'] middle sized 2D arrays
+    # ['Y'] and ['M'] small 2D suqare arrays
     # ['Y'] : np array, shape (43, 43), dtype('float64')
     # ['M'] : np array, (43, 43), dtype('float64')
-    infile = datadir("linmod_01_IN.pkl")
-    expfile = datadir("linmod_01_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_01_IN.h5")
+    expfile = datadir("linmod_01_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_02():
-    # similar to test_01, shapes of ['Y'] and ['M'] changed
+    # ['Y'] and ['M'] small 2D rectengular arrays
     # ['Y'] : np array, (62, 7), dtype('float64')
     # ['M'] : np array, (62, 92), dtype('float64')
-    infile = datadir("linmod_02_IN.pkl")
-    expfile = datadir("linmod_02_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_02_IN.h5")
+    expfile = datadir("linmod_02_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_03():
     # ['Y'] is a 3D array, ['M'] is a 2D array
     # ['Y'] : np array, (52, 64, 76), dtype('float64')
     # ['M'] : np array, (52, 2), dtype('float64')
-    infile = datadir("linmod_03_IN.pkl")
-    expfile = datadir("linmod_03_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_03_IN.h5")
+    expfile = datadir("linmod_03_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_04():
     # similar to test_03, shapes of ['Y'] and ['M'] changed
     # ['Y'] : np array, (69, 41, 5), dtype('float64')
     # ['M'] : np array, (69, 30), dtype('float64')
-    infile = datadir("linmod_04_IN.pkl")
-    expfile = datadir("linmod_04_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_04_IN.h5")
+    expfile = datadir("linmod_04_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_05():
-    # similar to test_01, shapes of ['Y'] and ['M'] changed
+    # ['Y'] and ['M'] small 2D rectengular arrays, size(Y) < size(M)
     # ['Y'] : np array, (81, 1), dtype('float64')
     # ['M'] : np array, (81, 2), dtype('float64')
-    infile = datadir("linmod_05_IN.pkl")
-    expfile = datadir("linmod_05_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_05_IN.h5")
+    expfile = datadir("linmod_05_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_06():
-    # similar to test_03, shapes of ['Y'] and ['M'] changed
+    # ['Y'] is a 3D array, ['M'] is a 2D array, M has more columns than Y
     # ['Y'] : np array, (93, 41, 57), dtype('float64')
     # ['M'] : np array, (93, 67), dtype('float64')
-    infile = datadir("linmod_06_IN.pkl")
-    expfile = datadir("linmod_06_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_06_IN.h5")
+    expfile = datadir("linmod_06_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_07():
-    # similar to test_03, shapes of ['Y'] and ['M'] changed
+    # similar to test_06, differently shaped Y and M
     # ['Y'] : np array, (40, 46, 21), dtype('float64')
     # ['M'] : np array, (40, 81), dtype('float64')
-    infile = datadir("linmod_07_IN.pkl")
-    expfile = datadir("linmod_07_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_07_IN.h5")
+    expfile = datadir("linmod_07_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_08():
@@ -136,63 +117,39 @@ def test_08():
     # ['Y'] : np array, (93, 43), dtype('float64')
     # ['M'] : np array, (93, 2), dtype('float64')
     # ['tri'] : np array, (93, 3), dtype('int64')
-    infile = datadir("linmod_08_IN.pkl")
-    expfile = datadir("linmod_08_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_08_IN.h5")
+    expfile = datadir("linmod_08_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_09():
-    # similar to test_03 + optional ['tri'] input for surf
+    # ['Y'] is 3D array, ['M'] is 2D array, M has more cols than Y, tri given
     # ['Y'] : np array, (98, 69, 60), dtype('float64')
     # ['M'] : np array, (98, 91), dtype('float64')
     # ['tri'] : np array, (60, 3), dtype('int64')
-    infile = datadir("linmod_09_IN.pkl")
-    expfile = datadir("linmod_09_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_09_IN.h5")
+    expfile = datadir("linmod_09_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_10():
     # similar to test_02 + optional ['lat'] input for surf
     # ['Y'] : np array, (49, 27), dtype('float64')
     # ['M'] : np array, (49, 2), dtype('float64')
-    # ['lat'] : np array, (3, 3, 3), dtype('bool')
-    infile = datadir("linmod_10_IN.pkl")
-    expfile = datadir("linmod_10_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    # ['lat'] : np array, (3, 3, 3), dtype('int64'), 1's or 0's
+    infile = datadir("linmod_10_IN.h5")
+    expfile = datadir("linmod_10_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_11():
     # similar to test_03 + optional ['lat'] input for surf
     # ['Y'] : np array, (45, 27, 3), dtype('float64')
     # ['M'] : np array, (45, 7), dtype('float64')
-    # ['lat'] : np array, (3, 3, 3), dtype('int64')
-    infile = datadir("linmod_11_IN.pkl")
-    expfile = datadir("linmod_11_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    # ['lat'] : np array, (3, 3, 3), dtype('int64'), 1's or 0's
+    infile = datadir("linmod_11_IN.h5")
+    expfile = datadir("linmod_11_OUT.h5")
+    dummy_test(infile, expfile)
 
 
 def test_12():
@@ -200,19 +157,9 @@ def test_12():
     # ['Y'] : np array, (10, 20484), dtype('float64')
     # ['age'] : np array, (1, 10), dtype('float64')
     # ['tri'] : np array, (40960, 3), dtype('int32')
-    # ['coord'] :np array, (3, 20484), dtype('float64')
-    infile = datadir("linmod_12_IN.pkl")
-    expfile = datadir("linmod_12_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    age = idic["age"]
-    AGE = Term(np.array(age), "AGE")
-    idic["M"] = 1 + AGE
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("thickness_n10.h5")
+    expfile = datadir("linmod_12_OUT.h5")
+    dummy_test(infile, expfile, simple=False)
 
 
 def test_13():
@@ -220,19 +167,9 @@ def test_13():
     # ['Y'] : np array, (10, 20484), dtype('float64')
     # ['age'] : np array, (1, 10), dtype('float64')
     # ['tri'] : np array, (40960, 3), dtype('int32')
-    # ['coord'] : np array, (3, 20484), dtype('float64')
-    infile = datadir("linmod_13_IN.pkl")
-    expfile = datadir("linmod_13_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    age = idic["age"]
-    AGE = Term(np.array(age), "AGE")
-    idic["M"] = 1 + AGE
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_13_IN.h5")
+    expfile = datadir("linmod_13_OUT.h5")
+    dummy_test(infile, expfile, simple=False)
 
 
 def test_14():
@@ -240,56 +177,16 @@ def test_14():
     # ['Y'] : np array, (10, 20484), dtype('float64')
     # ['age'] : np array, (1, 10), dtype('float64')
     # ['tri'] : np array, (40960, 3), dtype('int32')
-    # ['coord'] : np array, (3, 20484), dtype('float64')
-    infile = datadir("linmod_14_IN.pkl")
-    expfile = datadir("linmod_14_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    age = idic["age"]
-    AGE = Term(np.array(age), "AGE")
-    idic["M"] = 1 + AGE
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_14_IN.h5")
+    expfile = datadir("linmod_14_OUT.h5")
+    dummy_test(infile, expfile, simple=False)
 
 
 def test_15():
-    # choose ['Y']-values in range of [-1, 1], modeling from ['params'] & ['colnames']
+    # similar to test_12, ['Y'] size doubled + model params extended
     # ['Y'] : np array, (20, 20484), dtype('float64')
-    # ['params'] : np array, (20, 9), dtype('uint16')
-    # ['colnames'] : np array, (9,), dtype('<U11')
+    # ['M'] : np array, (20, 9), dtype('uint16')
     # ['tri'] : np array, (40960, 3), dtype('int32')
-    # ['coord'] : np array, (3, 20484), dtype('float64')
-    infile = datadir("linmod_15_IN.pkl")
-    expfile = datadir("linmod_15_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    params = idic["params"]
-    colnames = list(idic["colnames"])
-    idic["M"] = Term(params, colnames)
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
-
-
-def test_16():
-    # similar to test_15, modeling only using the ['params']
-    # ['Y'] : np array, (20, 20484), dtype('float64')
-    # ['params'] : np array, (20, 9), dtype('uint16')
-    # ['tri'] : np array, (40960, 3), dtype('int32')
-    # ['coord'] :np array, (3, 20484), dtype('float64')
-    infile = datadir("linmod_16_IN.pkl")
-    expfile = datadir("linmod_16_OUT.pkl")
-    ifile = open(infile, "br")
-    idic = pickle.load(ifile)
-    ifile.close()
-    params = idic["params"]
-    idic["M"] = Term(params)
-    ofile = open(expfile, "br")
-    oslm = pickle.load(ofile)
-    ofile.close()
-    dummy_test(idic, oslm)
+    infile = datadir("linmod_15_IN.h5")
+    expfile = datadir("linmod_15_OUT.h5")
+    dummy_test(infile, expfile)
