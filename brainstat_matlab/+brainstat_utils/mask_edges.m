a59e0d95dae5840c212192bcdc5b8951c0e6877d
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-04-15 10:05:15 -0400

    [DOC] brainstat.utils comments

--- brainstat_matlab/+brainstat_utils/mask_edges.m
+++ brainstat_matlab/+brainstat_utils/mask_edges.m
@@ -1,4 +1,12 @@
 function [edges, idx] = mask_edges(edges, mask)
+% MASK_EDGES    Remove edges with vertices outside the mask
+%   edges = MASK_EDGES(edges, mask) removes edges connecting vertices outside
+%   the mask. Returned edges have contiguous values. Edges is a e-by-2 matrix.
+%   mask is a v-by-1 loigcal vector where v is the number of vertices. 
+%
+%   [edges, idx] = MASK_EDGES(edges, mask) also returns the row indices of the
+%   edges that are kept.
+
 missing_edges = find(~mask);
 remove_edges = ismember(edges, missing_edges);
 
