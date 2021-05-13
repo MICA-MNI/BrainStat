afc9a56155d2a9e162a6fd3e0c0cc4fef2dcabd8
Şeyma Bayrak <sheymaba@gmail.com>
2021-05-13 15:22:28 +0200

    very simple test data generation for mesh_smooth, using sklearn ParameterGrid

---
+++ brainstat/tests/datagen_mesh_smooth.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pickle
+from sklearn.model_selection import ParameterGrid
+from brainstat.mesh.data import mesh_smooth
+from testutil import datadir
+
+
+def generate_smooth_out(rand_dict):
+    """Generates a valid SLM for a surface.
+    Parameters...
+    
+    """
+    # there are going to be input params for mesh_smooth
+    Y = rand_dict["Y"]
+    FWHM = rand_dict["FWHM"]
+    surf = {}
+    if "tri" in rand_dict.keys():
+        surf["tri"] = rand_dict["tri"]
+    if "lat" in rand_dict.keys():
+        surf["lat"] = rand_dict["lat"]
+   # this is going to be the output dict
+
+    O = {}
+    O["Python_Y"] = mesh_smooth(Y, surf, FWHM)
+
+    return O
+
+
+def params2files(rand_dict, O, test_num):
+    """Converts params to input/output files"""
+    # filenames for the input and outpur dict's
+    basename = "xstatsmo"
+    fin_name = datadir(basename + "_" + f"{test_num:02d}" + "_IN.pkl")
+    fout_name = datadir(basename + "_" + f"{test_num:02d}" + "_OUT.pkl")
+
+    # save input and output data in pickle format with filenames above
+    with open(fin_name, "wb") as f:
+        pickle.dump(rand_dict, f, protocol=4)
+    with open(fout_name, "wb") as g:
+        pickle.dump(O, g, protocol=4)
+    return
+
+
+np.random.seed(0)
+mygrid = [{"Y": [np.random.rand(72,72), np.random.rand(30,30,10)], 
+           "FWHM": [np.random.rand(), int(3.0)],
+           "tri": [np.random.randint(0, 10, size=(20,3))]},
+          {"Y": [np.random.rand(72,72), np.random.rand(30,30,10)], 
+           "FWHM": [np.random.rand(), int(3.0)],
+           "lat": [np.random.choice(a=[1, 0], size=(3,3,3))]}]
+
+myparamgrid = ParameterGrid(mygrid)
+
+#print(list(myparamgrid))
+
+# Test 1: Y square 2D array, FWHM in range [0, 1], tri (20,3)
+# Test 2: Y 3D array, FWHM in range [0, 1], tri (20,3)
+# Test 3: Y square 2D array, FWHM int(3), tri (20,3)
+# Test 4: Y 3D array, FWHM int(3), tri (20,3)
+# Test 5: Y square 2D array, FWHM in range [0, 1], lat (3,3,3)
+# Test 6: Y 3D array, FWHM in range [0, 1], lat (3,3,3)
+# Test 7: Y square 2D array, FWHM int(3), lat (3,3,3)
+# Test 8: Y 3D array, FWHM int(3), lat (3,3,3)
+
+
+test_num = 0
+for params in myparamgrid:
+    rand_dict = {}
+    for key in list(params.keys()):
+        rand_dict[key] = params[key]
+    O = generate_smooth_out(rand_dict)
+    test_num += 1
+    params2files(rand_dict, O, test_num)
+    
+    print('AAAAAAAA ', test_num, rand_dict["Y"].shape, rand_dict["FWHM"])
+
+
+
