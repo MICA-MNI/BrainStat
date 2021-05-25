e1b047449bf4b0d33091c69de78aa3518f0ea794
Şeyma Bayrak <sheymaba@gmail.com>
2021-05-25 20:45:27 +0200

    test data generation for test_mesh_normalize.py by using ParamGrids

---
+++ brainstat/tests/datagen_mesh_normalize.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+from testutil import datadir
+from nilearn import datasets
+from brainstat.mesh.data import mesh_normalize
+from sklearn.model_selection import ParameterGrid
+from brainstat.context.utils import read_surface_gz
+from brainspace.mesh.mesh_elements import get_cells
+
+
+def generate_mesh_normalize_out(I):
+    if "mask" not in I.keys():
+        I["mask"] = None
+    # run mesh_normalize
+    D = {}
+    D["Python_Y"], D["Python_Yav"] = mesh_normalize(I["Y"], I["mask"], subdiv="s")
+    return D
+
+
+def params2files(I, D, test_num):
+    """Converts params to input/output files"""
+    basename = "xstatnor"
+    fin_name = datadir(basename + "_" + f"{test_num:02d}" + "_IN.pkl")
+    fout_name = datadir(basename + "_" + f"{test_num:02d}" + "_OUT.pkl")
+    with open(fin_name, "wb") as f:
+        pickle.dump(I, f, protocol=4)
+    with open(fout_name, "wb") as g:
+        pickle.dump(D, g, protocol=4)
+    return
+
+
+pial_fs5 = datasets.fetch_surf_fsaverage()["pial_left"]
+pial_surf = read_surface_gz(pial_fs5)
+real_tri = np.array(get_cells(pial_surf))
+
+np.random.seed(0)
+
+mygrid = [
+    {
+        "Y": [
+            np.random.randint(1, 10, size=(1, 20)),
+            np.random.randint(1, 10, size=(2, 20)),
+            np.random.randint(2, 10, size=(3, 20, 4)),
+        ],
+        "mask": [None, np.random.choice(a=[False, True], size=(20,))],
+    },
+    {
+        "Y": [real_tri],
+        "mask": [None, np.random.choice(a=[False, True], size=(real_tri.shape[1]))],
+    },
+]
+
+myparamgrid = ParameterGrid(mygrid)
+
+# Here wo go!
+# Tests 1-4 : Y is 2D or 3D arrays type int, mask is None or random bool
+# Tests 6-8 : Y is pial_fs5 trinagles, mask is None or random bool
+
+test_num = 0
+for params in myparamgrid:
+    test_num += 1
+    I = {}
+    for key in params.keys():
+        I[key] = params[key]
+    D = generate_mesh_normalize_out(I)
+    params2files(I, D, test_num)
