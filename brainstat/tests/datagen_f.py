9eea15bd04624bac701bbce8da21c458e9307f0d
Şeyma Bayrak <sheymaba@gmail.com>
2021-05-25 19:31:24 +0200

    commentary for test numbers and contest & black styling

--- brainstat/tests/datagen_f.py
+++ brainstat/tests/datagen_f.py
@@ -20,17 +20,15 @@ def generate_random_two_slms(I):
             setattr(slm1, key[4:], I[key])
         elif "2" in key:
             setattr(slm2, key[4:], I[key])
-
     return slm1, slm2
 
 
 def generate_f_test_out(slm1, slm2):
     D = {}
     slm_OUT = f_test(slm1, slm2)
-    mykeys = ['X', 'df', 'SSE', 'coef', 'k', 't']
+    mykeys = ["X", "df", "SSE", "coef", "k", "t"]
     for key in mykeys:
         D[key] = getattr(slm_OUT, key)
-
     return D
 
 
@@ -43,9 +41,7 @@ def params2files(I, D, test_num):
         pickle.dump(I, f, protocol=4)
     with open(fout_name, "wb") as g:
         pickle.dump(D, g, protocol=4)
-
     return
-
 
 
 np.random.seed(0)
@@ -56,23 +52,30 @@ c = 100
 d = 42
 e = 77
 
-mygrid = [{"slm1X": [np.random.randint(0, a, size=(a,b)),
-                  np.random.rand(a,b)],
-            "slm1df": [int(a)],
-            "slm2df": [int(b)],
-            "slm1SSE": [np.random.randint(1,a,size=(c,1)),
-                        np.random.randint(1, a, size=(c,d))],
-            "slm2SSE": [np.random.randint(1,a,size=(c,1)),
-                        np.random.randint(1, a, size=(c,d))],
-            "slm1coef": [np.random.rand(e,d),
-                         np.random.rand(e,d,3)],
-            "slm2coef": [np.random.rand(e,d)]
-    }] 
+mygrid = [
+    {
+        "slm1X": [np.random.randint(0, a, size=(a, b)), np.random.rand(a, b)],
+        "slm1df": [int(a)],
+        "slm2df": [int(b)],
+        "slm1SSE": [
+            np.random.randint(1, a, size=(c, 1)),
+            np.random.randint(1, a, size=(c, d)),
+        ],
+        "slm2SSE": [
+            np.random.randint(1, a, size=(c, 1)),
+            np.random.randint(1, a, size=(c, d)),
+        ],
+        "slm1coef": [np.random.rand(e, d), np.random.rand(e, d, 3)],
+        "slm2coef": [np.random.rand(e, d)],
+    }
+]
 
-    
 myparamgrid = ParameterGrid(mygrid)
 
-
+# Here wo go!
+# Test 1-16 : slm1X 2D array type int or float64, slm1df and slm2df int,
+# slm1SSE and slm2SSE 2D arrays type int, slm1coef and slm2coef 2D or 3D
+# arrays type float64
 
 test_num = 0
 for params in myparamgrid:
@@ -80,9 +83,8 @@ for params in myparamgrid:
     I = {}
     for key in params.keys():
         I[key] = params[key]
-    # make "slm2X" equal to "slm1X" to avoid nested-model error        
+    # make "slm2X" equal to "slm1X" to avoid nested-model error
     I["slm2X"] = I["slm1X"]
     slm1, slm2 = generate_random_two_slms(I)
     D = generate_f_test_out(slm1, slm2)
-    params2files(I,D,test_num)
-
+    params2files(I, D, test_num)
