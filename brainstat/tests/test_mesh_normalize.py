6e2268c371944fcedb1a32975a39e09233bf844e
Şeyma Bayrak <sheymaba@gmail.com>
2021-05-25 20:46:02 +0200

    newly generated test data file names embedded

--- brainstat/tests/test_mesh_normalize.py
+++ brainstat/tests/test_mesh_normalize.py
@@ -37,35 +37,48 @@ def dummy_test(infile, expfile):
 
 
 def test_01():
-    # testing only with mandatory input ['Y']
-    # ['Y'] : 2D np array, shape (1, 2), dtype('int64')
-    infile = datadir("statnor_01_IN.pkl")
-    expfile = datadir("statnor_01_OUT.pkl")
+    infile = datadir("xstatnor_01_IN.pkl")
+    expfile = datadir("xstatnor_01_OUT.pkl")
     dummy_test(infile, expfile)
 
 
 def test_02():
-    # testing with optional input ['mask']
-    # ['Y'] : 2D np array, shape (1, 10), dtype('int64')
-    # ['mask'] : 1D np array, shape (10,), dtype('bool')
-    infile = datadir("statnor_02_IN.pkl")
-    expfile = datadir("statnor_02_OUT.pkl")
+    infile = datadir("xstatnor_02_IN.pkl")
+    expfile = datadir("xstatnor_02_OUT.pkl")
     dummy_test(infile, expfile)
 
 
 def test_03():
-    # increase size of ['Y'], keep optional input ['mask']
-    # ['Y'] : 2D np array, shape (2, 10), dtype('int64')
-    # ['mask'] : 1D np array, shape (10,), dtype('bool')
-    infile = datadir("statnor_03_IN.pkl")
-    expfile = datadir("statnor_03_OUT.pkl")
+    infile = datadir("xstatnor_03_IN.pkl")
+    expfile = datadir("xstatnor_03_OUT.pkl")
     dummy_test(infile, expfile)
 
 
 def test_04():
-    # increase dimension of ['Y'], keep optional input ['mask']
-    # ['Y'] : 3D np array, shape (3, 4, 2), dtype('float64')
-    # ['mask'] : 1D np array, shape (4,), dtype('bool')
-    infile = datadir("statnor_04_IN.pkl")
-    expfile = datadir("statnor_04_OUT.pkl")
+    infile = datadir("xstatnor_04_IN.pkl")
+    expfile = datadir("xstatnor_04_OUT.pkl")
+    dummy_test(infile, expfile)
+
+
+def test_05():
+    infile = datadir("xstatnor_05_IN.pkl")
+    expfile = datadir("xstatnor_05_OUT.pkl")
+    dummy_test(infile, expfile)
+
+
+def test_06():
+    infile = datadir("xstatnor_06_IN.pkl")
+    expfile = datadir("xstatnor_06_OUT.pkl")
+    dummy_test(infile, expfile)
+
+
+def test_07():
+    infile = datadir("xstatnor_07_IN.pkl")
+    expfile = datadir("xstatnor_07_OUT.pkl")
+    dummy_test(infile, expfile)
+
+
+def test_08():
+    infile = datadir("xstatnor_08_IN.pkl")
+    expfile = datadir("xstatnor_08_OUT.pkl")
     dummy_test(infile, expfile)
