0dd79246912bee00309305f4f61ee68a08c9a86a
Şeyma Bayrak <sheymaba@gmail.com>
2021-05-26 08:21:29 +0200

    add more params in Grid, tidy up, comment

--- brainstat/tests/datagen_stat_threshold.py
+++ brainstat/tests/datagen_stat_threshold.py
@@ -10,23 +10,30 @@ from brainstat.stats._multiple_comparisons import stat_threshold
 
 
 def generate_stat_threshold_out(I):
-    
     D = {}
-    D["peak_threshold"], D["extent_threshold"], D["peak_threshold_1"], \
-    D["extent_threshold_1"], D["t"], \
-    D["rho"] = stat_threshold(I["search_volume"],
-                              I["num_voxels"],
-                              I["fwhm"],
-                              I["df"],
-                              I["p_val_peak"],
-                              I["cluster_threshold"],
-                              I["p_val_extent"],
-                              I["nconj"],
-                              I["nvar"],
-                              None,
-                              None,
-                              I["nprint"])
+    (
+        D["peak_threshold"],
+        D["extent_threshold"],
+        D["peak_threshold_1"],
+        D["extent_threshold_1"],
+        D["t"],
+        D["rho"],
+    ) = stat_threshold(
+        I["search_volume"],
+        I["num_voxels"],
+        I["fwhm"],
+        I["df"],
+        I["p_val_peak"],
+        I["cluster_threshold"],
+        I["p_val_extent"],
+        I["nconj"],
+        I["nvar"],
+        None,
+        None,
+        I["nprint"],
+    )
     return D
+
 
 def params2files(I, D, test_num):
     """Converts params to input/output files"""
@@ -39,37 +46,45 @@ def params2files(I, D, test_num):
         pickle.dump(D, g, protocol=4)
     return
 
-# search_volume is a float, a list, 1D array
 
 np.random.seed(0)
 
 mygrid = [
-{'search_volume': [np.random.rand()*5,
-                   np.random.randint(1,20, size=(3,)).tolist(),],
-                   #np.random.rand(3,)*10,
-                   #np.random.rand(2,2)*20],
- 'num_voxels': [int(1), 
-                #np.random.randint(1,500),
-                np.random.randint(1,10, size=(3,)).tolist(),],
-                #np.random.rand(5,),
-                #np.random.rand(23,1)],
-                
- 'fwhm': [0.0, np.random.rand()*5],
- 'df': [5, np.random.randint(10,size=(2,2))],
- 'p_val_peak': [0.05, 
-                np.random.rand(4,).tolist(),],
-                #np.random.rand(7,),
-                #np.random.rand(5,5)],
- 'cluster_threshold': [0.001],
- 'p_val_extent': [0.05],
- 'nconj': [0.5],
- 'nvar': [1],
- 'nprint': [0]}
-
+    {
+        "search_volume": [
+            np.random.rand() * 5,
+            np.random.randint(1, 20, size=(3,)).tolist(),
+            np.random.rand(2, 2) * 20,
+        ],
+        "num_voxels": [
+            int(1),
+            np.random.randint(1, 10, size=(3,)).tolist(),
+        ],
+        "fwhm": [0.0, np.random.rand() * 5],
+        "df": [5, np.random.randint(10, size=(2, 2))],
+        "p_val_peak": [
+            0.05,
+            np.random.rand(
+                4,
+            ).tolist(),
+        ],
+        "cluster_threshold": [0.001],
+        "p_val_extent": [0.05],
+        "nconj": [0.5],
+        "nvar": [1],
+        "nprint": [0],
+    }
 ]
 
 myparamgrid = ParameterGrid(mygrid)
 
+
+# Here wo go!
+# Tests 1-48 : search_volume -> float, list, 2D array
+# num_voxel -> int, list of ints,
+# fwhm -> 0, float, df -> int, 2D array of type ints,
+# p_val_peak --> 0.05, list of floats
+# cluster_threshold, p_val_extent, nconj, nvar, nprint -> default values
 
 test_num = 0
 for params in myparamgrid:
@@ -78,5 +93,4 @@ for params in myparamgrid:
     for key in params.keys():
         I[key] = params[key]
     D = generate_stat_threshold_out(I)
-    params2files(I,D,test_num)
-    print('AAAA ', test_num)
+    params2files(I, D, test_num)
