55a136e00685c34df34f813e0754e3c6a8ae869e
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-06-07 10:31:59 -0400

    [TST] Add histology test

---
+++ brainstat/tests/test_histology.py
@@ -0,0 +1,22 @@
+"""Tests for histology."""
+
+import pytest
+from brainstat.context.histology import _get_urls
+import requests
+
+parametrize = pytest.mark.parametrize
+urls = _get_urls()
+
+@parametrize('template', list(urls.keys()))
+def test_urls(template):
+    """Tests whether the histology files can be downloaded.
+
+    Parameters
+    ----------
+    template : list
+        Template names. 
+    """
+    r = requests.head(urls[template])
+    assert r.status_code == 200
+
+
