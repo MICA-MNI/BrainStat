3964f68bc572bc91c913eb25a3412676655d9198
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-06-11 11:01:09 -0400

    update python_requires

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    python_requires="=3.6.*, =3.7.*, =3.8.*",
+    python_requires="==3.6.*, ==3.7.*, ==3.8.*",
     test_require=["pytest", "gitpython"],
     install_requires=[
         "abagen>=0.1",
