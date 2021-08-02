688fda1daa10a2ddaba266caef3aefec07ff6a20
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2021-08-02 14:03:56 -0400

    styling

--- brainstat/__init__.py
+++ brainstat/__init__.py
@@ -1,11 +1,11 @@
 """Neuroimaging statistics toolbox."""
-import warnings
 import sys
+import warnings
 
 __version__ = "0.2.2"
 
 if sys.version_info[1] == 6:
-    warnings.simplefilter('always', DeprecationWarning)
+    warnings.simplefilter("always", DeprecationWarning)
     warnings.warn(
         "Support for Python3.6 has been dropped. Future versions may not install on Python3.6.",
         DeprecationWarning,
