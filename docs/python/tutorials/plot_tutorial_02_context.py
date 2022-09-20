54f64d9ae6dbee56dbde047dc6e72319ebb7b6ed
Boris Bernhardt <boris.bernhardt@gmail.com>
2022-09-20 12:26:40 -0400

    update tuts2 after black was run

--- docs/python/tutorials/plot_tutorial_02_context.py
+++ docs/python/tutorials/plot_tutorial_02_context.py
@@ -443,33 +443,42 @@ plt.show()
 # It is equally possible to run context decoding on maps derived from e.g.
 # nilearn. In the example below, we decode task-fmri results from nilearn
 from nilearn.datasets import fetch_language_localizer_demo_dataset
+
 data_dir, _ = fetch_language_localizer_demo_dataset()
 
 from nilearn.glm.first_level import first_level_from_bids
-task_label = 'languagelocalizer'
-_, models_run_imgs, models_events, models_confounds = \
-    first_level_from_bids(
-        data_dir, task_label,
-        img_filters=[('desc', 'preproc')])
+
+task_label = "languagelocalizer"
+_, models_run_imgs, models_events, models_confounds = first_level_from_bids(
+    data_dir, task_label, img_filters=[("desc", "preproc")]
+)
 
 # obtain first level Model objects and arguments
 from nilearn.glm.first_level import first_level_from_bids
-task_label = 'languagelocalizer'
-_, models_run_imgs, models_events, models_confounds = \
-    first_level_from_bids(
-        data_dir, task_label,
-        img_filters=[('desc', 'preproc')])
+
+task_label = "languagelocalizer"
+_, models_run_imgs, models_events, models_confounds = first_level_from_bids(
+    data_dir, task_label, img_filters=[("desc", "preproc")]
+)
 
 import os
-json_file = os.path.join(data_dir, 'derivatives', 'sub-01', 'func',
-                         'sub-01_task-languagelocalizer_desc-preproc_bold.json')
+
+json_file = os.path.join(
+    data_dir,
+    "derivatives",
+    "sub-01",
+    "func",
+    "sub-01_task-languagelocalizer_desc-preproc_bold.json",
+)
 import json
-with open(json_file, 'r') as f:
-    t_r = json.load(f)['RepetitionTime']
+
+with open(json_file, "r") as f:
+    t_r = json.load(f)["RepetitionTime"]
 
 # project onto fsaverage
 from nilearn.datasets import fetch_surf_fsaverage
-fsa = fetch_surf_fsaverage(mesh='fsaverage5')
+
+fsa = fetch_surf_fsaverage(mesh="fsaverage5")
 
 import numpy as np
 from nilearn import surface
@@ -480,22 +489,27 @@ from nilearn.glm.contrasts import compute_contrast
 z_scores_right = []
 z_scores_left = []
 for (fmri_img, confound, events) in zip(
-        models_run_imgs, models_confounds, models_events):
+    models_run_imgs, models_confounds, models_events
+):
     texture = surface.vol_to_surf(fmri_img[0], fsa.pial_right)
     n_scans = texture.shape[1]
-    frame_times = t_r * (np.arange(n_scans) + .5)
+    frame_times = t_r * (np.arange(n_scans) + 0.5)
 
     # Create the design matrix
     #
     # We specify an hrf model containing Glover model and its time derivative.
     # The drift model is implicitly a cosine basis with period cutoff 128s.
     design_matrix = make_first_level_design_matrix(
-        frame_times, events=events[0], hrf_model='glover + derivative',
-        add_regs=confound[0])
+        frame_times,
+        events=events[0],
+        hrf_model="glover + derivative",
+        add_regs=confound[0],
+    )
 
     # Contrast specification
-    contrast_values = (design_matrix.columns == 'language') * 1.0 -\
-                      (design_matrix.columns == 'string')
+    contrast_values = (design_matrix.columns == "language") * 1.0 - (
+        design_matrix.columns == "string"
+    )
 
     # Setup and fit GLM.
     # Note that the output consists in 2 variables: `labels` and `fit`
@@ -503,8 +517,7 @@ for (fmri_img, confound, events) in zip(
     # `estimates` contains the parameter estimates.
     # We input them for contrast computation.
     labels, estimates = run_glm(texture.T, design_matrix.values)
-    contrast = compute_contrast(labels, estimates, contrast_values,
-                                contrast_type='t')
+    contrast = compute_contrast(labels, estimates, contrast_values, contrast_type="t")
     # We present the Z-transform of the t map.
     z_score = contrast.z_score()
     z_scores_right.append(z_score)
@@ -512,11 +525,11 @@ for (fmri_img, confound, events) in zip(
     # Do the left hemisphere exactly the same way.
     texture = surface.vol_to_surf(fmri_img, fsa.pial_left)
     labels, estimates = run_glm(texture.T, design_matrix.values)
-    contrast = compute_contrast(labels, estimates, contrast_values,
-                                contrast_type='t')
+    contrast = compute_contrast(labels, estimates, contrast_values, contrast_type="t")
     z_scores_left.append(contrast.z_score())
 
 from scipy.stats import ttest_1samp, norm
+
 t_left, pval_left = ttest_1samp(np.array(z_scores_left), 0)
 t_right, pval_right = ttest_1samp(np.array(z_scores_right), 0)
 
@@ -524,7 +537,7 @@ z_val_left = norm.isf(pval_left)
 z_val_right = norm.isf(pval_right)
 
 # and then do a similar decoding on the z_val vectors
-map = np.concatenate([z_val_left,z_val_right])
+map = np.concatenate([z_val_left, z_val_right])
 meta_analysis = meta_analytic_decoder("fsaverage5", map)
 print(meta_analysis)
 
